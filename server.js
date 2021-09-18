21754995bd045e07d74552dec664b5e36fe3a144
curtiskennedy <curtisdeankennedy@gmail.com>
2021-09-18 13:48:09 -0600

    sms server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  console.log(req.body.Body);
+
+  twiml.message('Helloooooooooo');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
