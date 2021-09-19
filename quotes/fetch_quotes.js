c59d4b1a20cd10db687f59b9815b3858f6e7a2db
Albert Dinh <84608403+albert-dinh-01@users.noreply.github.com>
2021-09-18 18:53:15 -0600

    fell out of scope after leaving the callback function

--- quotes/fetch_quotes.js
+++ quotes/fetch_quotes.js
@@ -1,10 +1,36 @@
 // return a quote and its author every request
-
 const request = require("request");
-const requestURL = "https://type.fit/api/quotes";
 
-request({ url: requestURL }, (error, response) => {
-	const responseData = JSON.parse(response.body);
-	const textContent = responseData[0].text;
-	const author = responseData[0].author;
+const getQuotesInfo = (callback) => {
+	const requestURL = "https://type.fit/api/quotes";
+	request({ url: requestURL, json: true }, (error, response) => {
+		var responseData = response.body;
+		// need the length of the body to generate random number
+		var maxLengthData = responseData.length - 1;
+		const minIndex = 0;
+		var randomIndex = Math.floor(
+			Math.random() * (maxLengthData - minIndex + 1) + minIndex
+		);
+		var content = responseData[randomIndex].text;
+		var author = responseData[randomIndex].author;
+		var objRet = {
+			content: content,
+			author: author
+		};
+		callback(objRet);
+	});
+};
+
+module.exports = getQuotesInfo;
+
+// --- DEBUGGING --- //
+let quoteObjName;
+let quoteObjContent;
+let output;
+
+getQuotesInfo(({ content, author }) => {
+	quoteObjContent = content;
+	quoteObjName = author;
+	output = "(" + String(quoteObjName) + ") " + String(quoteObjContent);
 });
+console.log(output);
--- server.js
+++ server.js
@@ -1,32 +1,47 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const bodyParser = require('body-parser');
-const quotes = require("./quotes/fetch_quotes.js")
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
+const getQuotesInfo = require("./quotes/fetch_quotes.js");
 const app = express();
-
+const port = 1337;
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+app.post("/sms", (req, res) => {
+	const twiml = new MessagingResponse();
+	let quoteObjName;
+	let quoteObjContent;
+	let output;
+	input = req.body.Body;
 
-  input = req.body.Body
-  console.log(input);
+	if (input == "/help" || input == "/Help") {
+		output = "This is a help message";
+	}
+	// this is the conditional for the quote part
+	else if (input == "/quotes") {
+		getQuotesInfo(({ content, author }) => {
+			quoteObjContent = content;
+			quoteObjName = author;
 
-  if (input == '/help' || input == '/Help') {
-    output = "This is a help message";
-  }
-  else {
-    output = "this is a generic message";
-  }
-  
-  console.log(input + "   " + output);
-  twiml.message(output);
+		});
+		output = String(
+			"(" + String(quoteObjName) + ") " + String(quoteObjContent)
+		);
+	} else {
+		output = "This is a generic message";
+	}
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+	twiml.message(output);
+	res.writeHead(200, { "Content-Type": "text/xml" });
+	res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+http.createServer(app).listen(port, () => {
+	console.log("Express server listening on port", port);
+});
+
+// This is for actually killing the port after using CTRL-C
+process.on("SIGINT", function () {
+	console.log("\nGracefully shutting down from SIGINT (Ctrl-C)!");
+	process.exit(1);
 });
